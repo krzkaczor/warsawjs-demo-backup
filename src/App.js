ac5a86002f6eebac71ff6216bb248c20ae038cdf
Krzysztof Kaczor <krzkaczor@gmail.com>
2019-08-14 00:24:08 +0200

    add logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 
 import "./App.css";
+import logo from "./logo.svg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="app-logo"/>
+
         <p>Hello WarsawJS!</p>
 
         <button onClick={onClickHandler}>Click me!</button>
