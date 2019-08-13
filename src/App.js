759f71a3306b2e5ee5beb55d99fe5e89edd3460a
Krzysztof Kaczor <krzkaczor@gmail.com>
2019-08-13 23:06:56 +0200

    add button with custom handler

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { upperCase } from "lodash";
+
+import "./App.css";
+import logo from "./logo.svg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello WarsawJS!
-        </p>
+        <p>Hello WarsawJS!</p>
+        
+        <button onClick={onClickHandler}>Click me!</button>
       </header>
     </div>
   );
+}
+
+function onClickHandler() {
+  alert(upperCase("warsaw js!"));
 }
 
 export default App;
